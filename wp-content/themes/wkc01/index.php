cc41091d80d9e0cb24cd76a8e03ddf6c270108e4
elliottoman <elliottoman@gmail.com>
2019-02-28 16:51:00 -0500

    Add frontpage template

--- wp-content/themes/wkc01/index.php
+++ wp-content/themes/wkc01/index.php
@@ -2,14 +2,10 @@
 
 <!-- Index Template -->
 
-<?php if (is_front_page()) { get_template_part('snippets/homepage_carousel'); } ?>
-
 <main>
     <div class="post-content">
         <?php get_template_part( 'loop' ); ?>
     </div>
 </main>
-
-<?php if (is_front_page()) { dynamic_sidebar( 'home_instagram' ); } ?>
 
 <?php get_footer(); ?>
