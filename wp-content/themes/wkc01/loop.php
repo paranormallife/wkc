64358d63cbdfd9d97a370b11ea102541cd1f36ff
elliottoman <elliottoman@gmail.com>
2019-02-28 15:17:31 -0500

    Page styles

--- wp-content/themes/wkc01/loop.php
+++ wp-content/themes/wkc01/loop.php
@@ -1,8 +1,13 @@
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post();
+$thumb = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail', false, '' ); ?>
+
+<div class="post-featured-image">
+    <img src="<?php echo $thumb[0]; ?>" title="<?php the_title(); ?>" />
+</div>
 
 <article>
-<h1><?php the_title(); ?></h1>
-<?php the_content(); ?>        
+    <h1><?php the_title(); ?></h1>
+    <?php the_content(); ?>        
 </article>
 
 <?php endwhile; else: ?>
