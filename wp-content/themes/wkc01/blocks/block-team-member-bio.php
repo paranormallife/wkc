76ae2546caaddc76f95cae0334af1da091125660
elliottoman <elliottoman@gmail.com>
2019-03-07 10:56:48 -0500

    Team member bio updates

--- wp-content/themes/wkc01/blocks/block-team-member-bio.php
+++ wp-content/themes/wkc01/blocks/block-team-member-bio.php
@@ -1,6 +1,15 @@
 <div class="team-member">
+    <?php
+        $email = block_value('bio-email');
+        $name = block_value('bio-name');
+    ?>
     <div class="bio-image">
-        <img src="<?php block_field('bio-image') ?>" alt="<?php block_field('bio-name'); ?>" />
+        <img src="<?php block_field('bio-image') ?>" alt="<?php echo $name; ?>" />
     </div>
-    <h3><?php block_field('bio-name'); ?></h3>
+    <?php if( $email != '' ) { ?>
+        <h3><a href="mailto: <?php block_field('bio-email');?>" title="Send an email to <?php echo $name; ?>"><i class="fas fa-envelope"></i></a> <?php echo $name; ?></h3>
+    <?php } else { ?>
+        <h3><?php echo $name; ?></h3>
+    <?php } ?>
+    <div class="bio-blurb"><?php block_field('bio-blurb'); ?></div>
 </div>
