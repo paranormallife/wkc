ba21e5da8878219b124690f9a4e43f445749a0f0
elliottoman <elliottoman@gmail.com>
2019-04-11 14:55:32 -0400

    Redirects plugin

---
+++ wp-content/plugins/eps-301-redirects/templates/admin-tab-error.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ *
+ * The Redirects Tab.
+ *
+ * The main admin area for the redirects tab.
+ *
+ * @package    EPS 301 Redirects
+ * @author     WebFactory Ltd
+ */
+?>
+
+
+<div class="wrap">
+    <?php do_action('eps_redirects_admin_head'); ?>
+
+    <div class="eps-notice eps-warning">
+        <?php echo $options['description']; ?>
+    </div>
+
+
+    <div class="right">
+        <?php do_action('eps_redirects_panels_right'); ?>
+    </div>
+    <div class="left">
+        <?php do_action('eps_redirects_panels_left'); ?>
+    </div>
+</div> 
