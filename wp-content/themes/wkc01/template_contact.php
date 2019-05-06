f058312a5c413b979c8630544bd4b2220117d09c
elliottoman <elliottoman@gmail.com>
2019-05-06 09:51:41 -0400

    Contact page update

--- wp-content/themes/wkc01/template_contact.php
+++ wp-content/themes/wkc01/template_contact.php
@@ -4,7 +4,14 @@
 
 <main>
     <div class="post-content">
-        <?php get_template_part( 'loop' ); ?>
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <article>
+            <h1><?php the_title(); ?></h1>
+            <?php the_content(); ?>        
+        </article>
+        <?php endwhile; else: ?>
+        <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+        <?php endif; ?>
     </div>
 </main>
 
